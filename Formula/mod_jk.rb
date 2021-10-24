04dfbc71581356ff1de97b2b0afa29e58a69c961
Roman Khmelichek <roman.khmelichek@gmail.com>
2021-10-23 23:51:10 -0400

    Upgrade mod_jk to 1.2.48

--- Formula/mod_jk.rb
+++ Formula/mod_jk.rb
@@ -1,8 +1,8 @@
 class ModJk < Formula
   desc "An Apache HTTP Server module (mod_jk) for connecting to backends via the AJP protocol"
   homepage "http://tomcat.apache.org/connectors-doc/"
-  url "https://archive.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.46-src.tar.gz"
-  sha256 "7e1d520e1d1dacd042087ae52be7aae47a093b93cf26931827724aa8ab66cbe9"
+  url "https://archive.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.48-src.tar.gz"
+  sha256 "cb1b360ba0a12b2dbec119b60f561e9f657ed75df8188e5d902534b56b908e97"
 
   depends_on "httpd"
   depends_on "apr-util" => :build
