873c7ae19d6cf548e80d1969b2f158ffc3ebb8f0
Roman Khmelichek <roman.khmelichek@gmail.com>
2021-10-23 23:24:47 -0400

    Fix sha256

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -2,7 +2,7 @@ class Sshpass < Formula
   desc "Non-interactive ssh password auth"
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://sourceforge.net/projects/sshpass/files/sshpass/1.08/sshpass-1.08.tar.gz"
-  sha256 "03223d0fbe27bc42c08ae2995152d487bd00aa1e0350e887cc64abc5412c5daf"
+  sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
   def install
     ENV.O2
