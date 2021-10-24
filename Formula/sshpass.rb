208f8e16ea6d1c653e9558f2c1fbda1610363ecf
Roman Khmelichek <roman.khmelichek@gmail.com>
2021-10-23 23:19:15 -0400

    Upgrade sshpass to 1.0.8

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
   desc "Non-interactive ssh password auth"
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz"
-  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.08/sshpass-1.08.tar.gz"
+  sha256 "03223d0fbe27bc42c08ae2995152d487bd00aa1e0350e887cc64abc5412c5daf"
 
   def install
     ENV.O2
