11980facd126a53cd1d549e93b6d803ea0e6990f
Roman Khmelichek <roman.khmelichek@gmail.com>
2021-10-23 23:43:15 -0400

    ENV.O2 has been removed from Homebrew

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -5,7 +5,6 @@ class Sshpass < Formula
   sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
   def install
-    ENV.O2
     args = %W[
       --prefix=#{prefix}
       --disable-debug
