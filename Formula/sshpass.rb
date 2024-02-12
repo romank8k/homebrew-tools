57a2243cdd07ca4d6d1765c112160d02f9052af1
Roman Khmelichek <roman.khmelichek@gmail.com>
2024-02-12 16:52:13 -0500

    Update sshpass

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
   desc "Non-interactive ssh password auth"
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.08/sshpass-1.08.tar.gz"
-  sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
+  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.10/sshpass-1.10.tar.gz"
+  sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
 
   def install
     args = %W[
